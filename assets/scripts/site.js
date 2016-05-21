e6d2582fbd807b0af0a93850363263adf81b4f67
Caleb Evans <caleb@calebevans.me>
2016-05-20 20:18:57 -0700

    Add frontend scripts

---
+++ assets/scripts/site.js
@@ -0,0 +1,33 @@
+(function ( window, $, FastClick ) {
+jQuery(document).ready(function () {
+
+	// Cache jQuery elements
+	var $$ = {};
+	$$.menuToggle = $( '#site-header-nav-toggle' );
+	$$.siteHeaderNav = $( '#site-header-nav' );
+	$$.siteHeaderNavList = $( '#site-header-nav-list' );
+
+	// Toggle nav menu when nav control is clicked
+	$$.menuToggle.on( 'click', function () {
+		$$.siteHeaderNav.toggleClass('site-header-nav-open');
+	});
+
+	// Do not visit on first click nav items which have submenus
+	$$.siteHeaderNavList.on( 'click', 'a', function () {
+		var $link = $(this),
+			$subMenu = $link.next( '.sub-menu' );
+		// If nav menu is open and sub menu exists and sub menu is not open
+		if ( $$.siteHeaderNav.hasClass( 'site-header-nav-open' ) && 1 === $subMenu.length && false === $subMenu.hasClass( 'sub-menu-open' ) ) {
+			// Close any open submenus
+			$( '.sub-menu-open' ).removeClass( 'sub-menu-open' );
+			// Open submenu for clicked link
+			$subMenu.addClass( 'sub-menu-open' );
+			return false;
+		}
+	});
+
+	// Enable FastClick“
+	FastClick.attach( $('body')[0] );
+
+});
+}( window, jQuery, window.FastClick ));
