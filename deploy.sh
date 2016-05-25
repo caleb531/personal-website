a230a245adcdc4a16c6c66f8a72b6641dd4d3dc5
Caleb Evans <caleb@calebevans.me>
2016-05-24 18:11:40 -0700

    Add production deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# LOCAL_BUILD_DIR must end with a /
+LOCAL_BUILD_DIR="$(mktemp -d)"/_site/
+# REMOTE_PROD_DEST_DIR is relative to ~ on remote
+REMOTE_PROD_DEST_DIR=./public_html
+
+# Build production site to temporary build directory
+JEKYLL_ENV=production jekyll build --destination "$LOCAL_BUILD_DIR"
+
+# Copy local production build directory to remote
+rsync \
+  --archive \
+  --checksum \
+  --rsh "ssh -p $SSH_PORT" \
+  --verbose \
+  "$LOCAL_BUILD_DIR" \
+  "$SSH_USER"@"$SSH_HOSTNAME":"$REMOTE_PROD_DEST_DIR"
+
+# Remove local production directory when finished
+rm -rf "$LOCAL_BUILD_DIR"
