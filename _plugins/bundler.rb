fa8277ed9b5067305436719b6334c1bb80812bd7
Caleb Evans <caleb@calebevans.me>
2016-06-23 18:55:50 -0700

    Load all Jekyll packages dynamically via Bundler
    This eliminates the need to manually list all Jekyll packages in
    _config.yml when they're already listed in Gemfile.

---
+++ _plugins/bundler.rb
@@ -0,0 +1,5 @@
+require "rubygems"
+require "bundler/setup"
+# Automatically enable all Jekyll plugins listed in Gemfile (thereby eliminating
+# the need to manually list them in _config.yml)
+Bundler.require(:default)
