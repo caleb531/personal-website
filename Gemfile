8514e8e2f880d8a9305b3e4e08f78a0622cb8614
Caleb Evans <caleb@calebevans.me>
2016-06-23 20:18:25 -0700

    Replace bundler.rb with :jekyll_plugins group

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
-gem 'jekyll', '~>3.0'
-gem 'jekyll-seo-tag'
-gem 'jekyll-sitemap'
+
+group :jekyll_plugins do
+  gem 'jekyll', '~>3.0'
+  gem 'jekyll-seo-tag'
+  gem 'jekyll-sitemap'
+end
--- _plugins/bundler.rb
+++
@@ -1,5 +0,0 @@
-require "rubygems"
-require "bundler/setup"
-# Automatically enable all Jekyll plugins listed in Gemfile (thereby eliminating
-# the need to manually list them in _config.yml)
-Bundler.require(:default)
