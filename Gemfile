17ac4336d972991325ecee2548ea9a90949e57a8
Caleb Evans <caleb@calebevans.me>
2018-11-11 19:56:35 -0800

    Automate screenshot generation for portfolio websites

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ group :jekyll_plugins do
   gem 'jekyll-assets'
   gem 'sprockets', '~> 4.0.beta'
   gem 'uglifier'
+  gem 'front_matter_parser'
 
   gem 'jekyll-email-protect'
   gem 'jekyll-responsive_image'
---
+++ utilities/generate_website_screenshots.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'front_matter_parser'
+
+chrome_path = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+website_image_dir = 'assets/images/websites'
+website_image_extension='jpg'
+window_width = 1024
+window_height = 640
+
+# Create website image directory if it doesn't already exist
+FileUtils.mkdir_p website_image_dir
+
+# Generate screenshot for each portfolio website that has configuration
+Dir.glob('_websites/*.md') do |website_config_file|
+
+  website_name = File.basename(website_config_file, '.md')
+  puts "generating screenshot for #{website_name}"
+
+  parsed = FrontMatterParser::Parser.parse_file(website_config_file)
+  system("""#{chrome_path} \
+    --headless \
+    --disable-gpu \
+    --window-size=#{window_width},#{window_height} \
+    --hide-scrollbars \
+    --screenshot=#{website_image_dir}/#{website_name}.#{website_image_extension} \
+    #{parsed['direct_url']}
+  """)
+
+end
