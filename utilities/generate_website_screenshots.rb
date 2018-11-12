d524bbad03e9ce08f2d1d9049aa05b5e952608a7
Caleb Evans <caleb@calebevans.me>
2018-11-11 21:32:33 -0800

    Delete resized image dir when running screenshot tool

--- utilities/generate_website_screenshots.rb
+++ utilities/generate_website_screenshots.rb
@@ -5,12 +5,15 @@ require 'front_matter_parser'
 
 chrome_path = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 website_image_dir = 'assets/images/websites'
+resize_image_dir = 'assets/resized'
 website_image_extension='jpg'
 window_width = 1024
 window_height = 640
 
 # Create website image directory if it doesn't already exist
-FileUtils.mkdir_p website_image_dir
+FileUtils.mkdir_p(website_image_dir)
+# Remove existing directory containing resized images
+FileUtils.remove_dir(resize_image_dir, force=true)
 
 # Generate screenshot for each portfolio website that has configuration
 Dir.glob('_websites/*.md') do |website_config_file|
