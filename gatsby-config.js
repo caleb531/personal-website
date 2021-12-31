6a2c75f18ad9421225f49f1d0b9d2718e000b00d
Caleb Evans <caleb@calebevans.me>
2021-12-31 12:24:55 -0800

    Eliminate __dirname in Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
-        path: `${__dirname}/src/pages`
+        path: './src/pages'
       }
     }
   ]
