8c6cb5ef082b902e5a76814d3c2b3358bd0cab58
Caleb Evans <caleb@calebevans.me>
2022-01-01 11:40:42 -0800

    Build out navigation

--- src/templates/Navigation.tsx
+++ src/templates/Navigation.tsx
@@ -1,22 +1,29 @@
 import { graphql, useStaticQuery } from 'gatsby';
+import { keyBy } from 'lodash-es';
 import React from 'react';
 import navigation from '../data/navigation.json';
 
 export type PageFrontmatter = { title: string, slug: string, id: string };
 export type PageData = { frontmatter: PageFrontmatter };
+export type PageMap = { [key: string]: PageData };
 
 function Navigation() {
 
   const data = useStaticQuery(query);
-  const pages = data.allMdx.edges
-    .map(({ node }: { node: PageData }) => node.frontmatter);
+  const allPages = data.allMdx.edges.map((edge: { node: PageData }) => {
+    return edge.node;
+  });
+  const pagesById: PageMap = keyBy(allPages, (page: PageData) => {
+    return page.frontmatter.id;
+  });
 
   return (
     <nav id="site-header-nav">
       <ul id="site-header-nav-list">
-        {pages.filter((page: PageFrontmatter) => navigation.includes(page.id)).map((page: PageFrontmatter) => {
+        {navigation.map((pageId: string) => {
+          const { slug, title } = pagesById[pageId].frontmatter;
           return (
-            <li key={page.id}><a href={page.slug}>{page.title}</a></li>
+            <li key={pageId}><a href={slug}>{title}</a></li>
           );
         })}
       </ul>
