30871233476a70b670bbe013f02ff3b2608124ef
Caleb Evans <caleb@calebevans.me>
2022-01-01 12:07:31 -0800

    Add Google Fonts to Head template

--- src/templates/Head.tsx
+++ src/templates/Head.tsx
@@ -18,6 +18,9 @@ function Head({ pageTitle }: Props) {
         <title>{pageTitle} | {title}</title> :
         <title>{title} | {tagline}</title>
       }
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400&amp;display=swap" rel="stylesheet" />
       <link rel="shortcut icon" href={getGravatarUrl(email, 32)} />
       <link rel="icon" href={getGravatarUrl(email, 192)} sizes="192x192" />
       <meta name="twitter:image" content={getGravatarUrl(email, 250)} />
