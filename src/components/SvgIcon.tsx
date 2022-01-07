13b9ca8476ff00d488a94e951ac3de65a8d94de0
Caleb Evans <caleb@calebevans.me>
2022-01-06 19:49:31 -0800

    Add note about security of SvgIcon component

--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -6,6 +6,8 @@ function SvgIcon({ content }: Props) {
   return (
     <div
       className="svg-icon-wrapper"
+      // Because the SVG content is coming from my own server, it is relatively
+      // safe to insert into the DOM with dangerouslySetInnerHTML
       dangerouslySetInnerHTML={{ __html: content }} />
   );
 }
