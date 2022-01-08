6dbf7c689a9928aaeaa859cd9ced05b6dd4703e0
Caleb Evans <caleb@calebevans.me>
2022-01-08 15:55:53 -0800

    Rewrite screenshot utility to be more asynchronous

--- utilities/screenshot-website.ts
+++ utilities/screenshot-website.ts
@@ -1,6 +1,6 @@
 import fm from 'front-matter';
 import fs from 'fs';
-import glob from 'glob';
+import glob from 'glob-promise';
 import path from 'path';
 import puppeteer from 'puppeteer';
 
@@ -13,17 +13,13 @@ const windowWidth = 1024;
 const windowHeight = 640;
 
 // Create website image directory if it doesn't already exist
-fs.mkdir(websiteImageDir, { recursive: true }, () => {/* noop */});
-
-// Generate screenshot for the specified file path(s); otherwise, default to
-// every file in the /src/websites directory
-let websiteConfigFilePaths = process.argv.slice(2);
-if (websiteConfigFilePaths.length === 0) {
-  websiteConfigFilePaths = glob.sync('src/websites/*.md');
+function createWebsiteImageDirectory() {
+  return fs.promises.mkdir(websiteImageDir, { recursive: true });
 }
 
 async function generateScreenshots(websiteConfigFilePaths: string[]) {
 
+  await createWebsiteImageDirectory();
   const browser = await puppeteer.launch();
 
   // Generate screenshot for each portfolio website that has configuration
@@ -31,29 +27,28 @@ async function generateScreenshots(websiteConfigFilePaths: string[]) {
 
     const websiteName = path.basename(websiteConfigFilePath, '.md');
 
-    await fs.promises.readFile(websiteConfigFilePath, 'utf8').then(async (websiteConfigFileContents: string) => {
+    const websiteConfigFileContents = await fs.promises.readFile(websiteConfigFilePath, 'utf8');
 
-      const websiteFrontmatter = fm(websiteConfigFileContents).attributes as Frontmatter;
-      const websiteImagePath = path.join(websiteImageDir, `${websiteName}.${websiteImageExtension}`);
+    const websiteFrontmatter = fm(websiteConfigFileContents).attributes as Frontmatter;
+    const websiteImagePath = path.join(websiteImageDir, `${websiteName}.${websiteImageExtension}`);
 
-      const page = await browser.newPage();
-      await page.setViewport({
-        width: windowWidth,
-        height: windowHeight
-      });
-      await page.goto(websiteFrontmatter.direct_url);
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: windowWidth,
+      height: windowHeight
+    });
+    await page.goto(websiteFrontmatter.direct_url);
 
-      return page.screenshot({
-        path: websiteImagePath,
-        type: 'jpeg',
-        quality: websiteImageQuality,
-        captureBeyondViewport: false
-      }).then(() => {
-        console.log(`generated screenshot for ${websiteName}`);
-      }).catch((error) => {
-        console.error(error);
-      });
-
+    return page.screenshot({
+      path: websiteImagePath,
+      type: 'jpeg',
+      quality: websiteImageQuality,
+      captureBeyondViewport: false
+    }).then(() => {
+      console.log(`generated screenshot for ${websiteName}`);
+    }).catch((error) => {
+      console.error(error);
+      return error;
     });
 
   }));
@@ -61,4 +56,14 @@ async function generateScreenshots(websiteConfigFilePaths: string[]) {
   await browser.close();
 
 }
-generateScreenshots(websiteConfigFilePaths);
+
+async function main() {
+  // Generate screenshot for the specified file path(s); otherwise, default to
+  // every file in the /src/websites directory
+  let websiteConfigFilePaths = process.argv.slice(2);
+  if (websiteConfigFilePaths.length === 0) {
+    websiteConfigFilePaths = await glob.promise('src/websites/*.md');
+  }
+  generateScreenshots(websiteConfigFilePaths);
+}
+main();
