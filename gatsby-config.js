4df09e4870309c32636bb357e68f760da64d113d
Caleb Evans <caleb@calebevans.me>
2022-01-15 10:51:05 -0800

    Add comment about esbuild register() call

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 const { register } = require('esbuild-register/dist/node');
 
+// Enable Gatsby to recognize TypeScript gatsby-* API files
 register({ target: 'node16' });
 
 module.exports = require('./gatsby-config.ts');
