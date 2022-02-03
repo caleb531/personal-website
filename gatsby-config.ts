d30026e72c1c3fb7d0ad30ba4d215dbb9d8e1a1b
Caleb Evans <caleb@calebevans.me>
2022-02-02 21:25:16 -0800

    WIP: try out new website design

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -97,7 +97,7 @@ export const plugins: GatsbyConfig['plugins'] = [
     resolve: 'gatsby-plugin-web-font-loader',
     options: {
       google: {
-        families: ['Noto Sans JP:300,400&display=swap']
+        families: ['M PLUS Rounded 1c:400,500&display=swap']
       }
     }
   },
