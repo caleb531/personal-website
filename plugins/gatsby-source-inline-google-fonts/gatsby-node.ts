8a23575ca50589968a9dace3c90c4afa2f863b99
Caleb Evans <caleb@calebevans.me>
2022-02-05 09:56:19 -0800

    Fetch Google Font CSS via source plugin

--- plugins/gatsby-source-inline-google-fonts/gatsby-node.ts
+++ plugins/gatsby-source-inline-google-fonts/gatsby-node.ts
@@ -1,8 +1,54 @@
-import { GatsbyNode } from 'gatsby';
+import { GatsbyNode, NodeInput, PluginOptions } from 'gatsby';
+import fetch from 'node-fetch';
 
-// Read the contents of SVG files (represented by GraphQL nodes) so that the
-// <svg> contents can be referenced later for inlining in JSX (source:
-// https://stackoverflow.com/a/58151834/560642)
-export const sourceNodes: GatsbyNode['sourceNodes'] = ({ actions, createNodeId, createContentDigest, cache }) => {
-  // TODO
+// Constants for the font content type
+const FONT_NODE_TYPE = 'GoogleFontCss';
+
+// Node types for this plugin
+interface FontData {
+  url: string;
+  css: string;
+}
+interface FontPluginOptions extends PluginOptions {
+  fontCssUrls: string[];
+}
+type SourceNodesArgs = Parameters<GatsbyNode['sourceNodes']>[0];
+
+// Pre-fetches Google Font CSS URLs during the Gatsby build step, and saves the
+// CSS contents to GraphQL so they can be inlined as <style> tags with
+// react-helmet (documentation on creating a source plugin:
+// https://www.gatsbyjs.com/docs/how-to/plugins-and-themes/creating-a-source-plugin/)
+export const sourceNodes = (
+  { actions, createNodeId, createContentDigest, cache }: SourceNodesArgs,
+  pluginOptions: FontPluginOptions
+) => {
+  const { createNode } = actions;
+
+  async function fetchFontCss(fontCssUrl: string): Promise<string> {
+    return (await fetch(fontCssUrl)).text();
+  }
+
+  return Promise.all(pluginOptions.fontCssUrls.map(async (fontCssUrl) => {
+
+    const fontData: FontData = {
+      url: fontCssUrl,
+      css: await fetchFontCss(fontCssUrl)
+    };
+    const nodeMeta: NodeInput = {
+      id: createNodeId(`${FONT_NODE_TYPE}-${fontCssUrl}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: FONT_NODE_TYPE,
+        mediaType: 'text/css',
+        content: JSON.stringify(fontData),
+        contentDigest: createContentDigest(fontData)
+      },
+      url: fontCssUrl
+    };
+    const node = Object.assign({}, fontData, nodeMeta);
+    createNode(node);
+
+  }));
+
 };
