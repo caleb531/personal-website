31af4ac9b77fb26c7f686dc4fc04b3e35c474347
Caleb Evans <caleb@calebevans.me>
2022-02-12 12:40:03 -0800

    Add newlines to getEntriesOfType for readability

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,12 +5,15 @@ import path from 'path';
 import { Entry } from '../components/types';
 
 export function getEntriesOfType(entryType: string): Entry[] {
+
   const entryDirectory = path.join(process.cwd(), 'src', entryType);
   const entryPaths = glob.sync(`${entryDirectory}/*.md`);
+
   return entryPaths.map((entryPath) => {
     return {
       id: path.basename(entryPath, '.md'),
       ...matter(fs.readFileSync(entryPath)).data
     } as Entry;
   });
+
 }
