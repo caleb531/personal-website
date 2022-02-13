5f2eed988880a174918347361ab796ae5fa541b3
Caleb Evans <caleb@calebevans.me>
2022-02-12 16:22:54 -0800

    Fix issues with migrated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import site from '../data/site.json';
+import { withGlobalStaticProps } from '../lib/api';
 
 function PageNotFound() {
   return (
     <div className="page-404">
       <p>Sorry, I couldn't find the page you were looking for.
-      Please [send me an email](mailto:caleb@calebevans.me) explaining how you got here and I'll look into it. :)</p>
+      Please <a href={`mailto:${site.email}`}>send me an email</a> explaining how you got here and I'll look into it. :)</p>
 
-      <p>In the meantime, watch some [adorable red pandas playing in the snow](https://www.youtube.com/watch?v=y6GaPkkGZGw)!</p>
+      <p>In the meantime, watch some <a href="https://www.youtube.com/watch?v=y6GaPkkGZGw">adorable red pandas playing in the snow</a>!</p>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return withGlobalStaticProps({
+    props: {
+      title: 'Well, this is awkward.'
+    }
+  });
 }
 
 export default PageNotFound;
