ff153d7fc7350b985f0cd1ee5f0bfd4ad1a4e873
Caleb Evans <caleb@calebevans.me>
2022-02-12 18:22:43 -0800

    Migrate Google Analytics to NextJS

---
+++ src/components/useGoogleAnalytics.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+import site from '../data/site.json';
+
+// The useGoogleAnalytics() hook is a simple abstraction around the react-ga
+// library for loading and firing Google Analytics
+function useGoogleAnalytics() {
+  // Initialize Google Analytics on the initial page load
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ReactGA.initialize(site.googleAnalytics);
+    }
+  }, []);
+  // Trigger a pageview even on the initial page load and when navigating to
+  // another page on the site
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  });
+}
+export default useGoogleAnalytics;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import Layout from '../components/Layout';
+import useGoogleAnalytics from '../components/useGoogleAnalytics';
 import '../styles/index.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useGoogleAnalytics();
   return (
     <Layout {...pageProps}>
       <Component {...pageProps} />
