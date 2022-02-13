3d587f5f55875ba416cc8893768cfcb0a141c1a9
Caleb Evans <caleb@calebevans.me>
2022-02-12 20:56:30 -0800

    Add custom 500 Page to fix gravatar error
    Building the site for production was generating an 'Error occurred
    prerendering page "/500"' and 'Invalid URL: undefined' error. This
    appears to be caused by the automatically-generated 500 page not loading
    the global static props, thus resulting in the gravatarUrl prop being
    undefined.

---
+++ src/pages/500.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { withGlobalStaticProps } from '../lib/page';
+
+function PageNotFound() {
+  return (
+    <div className="page-500">
+      <p>Sorry, my server errored out. Please <a href="mailto:caleb@calebevans.me">email me</a> with the URL page you were trying to access, and I'll check it out.</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  return withGlobalStaticProps({
+    props: {
+      title: 'Internal Server Error'
+    }
+  });
+}
+
+export default PageNotFound;
