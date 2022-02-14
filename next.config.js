544b5d24e20786a3334b20da90712ea0a0ca1d4d
Caleb Evans <caleb@calebevans.me>
2022-02-14 12:09:19 -0800

    Attempt to fix broken /projects/ redirect
    It was undesirably redirecting from
    https://staging.calebevans.me/projects/ (no subpath) to the root of
    https://projects.calebevans.me/.

--- next.config.js
+++ next.config.js
@@ -14,8 +14,8 @@ const nextConfig = {
         permanent: true
       },
       {
-        source: '/projects/:id*',
-        destination: 'https://projects.calebevans.me/:id*',
+        source: '/projects/:id([A-Za-z0-9\-\/]+)',
+        destination: 'https://projects.calebevans.me/:id',
         permanent: true
       }
     ];
