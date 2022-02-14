8513282a8d1595eb13cf8a21f76da457051d27d6
Caleb Evans <caleb@calebevans.me>
2022-02-13 17:14:27 -0800

    Revise About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,23 +7,23 @@ function About() {
 
       <h3>Who are you?</h3>
 
-      <p>I'm Caleb Evans, a web developer with many passions: programming, spending time with family and friends, watching (and quoting) movies, trampolining, and eating quality pizza. I currently reside in Carlsbad, California, having just finished my Bachelor's in Computer Science at CSU San Marcos.</p>
+      <p>I'm Caleb Evans, a web developer from Carlsbad, California with many passions: programming, spending time with family and friends, watching (and quoting) movies, trampolining, and eating quality pizza.</p>
 
       <h3>What do you do?</h3>
 
-      <p>Since 2010, I've been programming for school and for fun. My most prominent work includes a plethora of web apps and useful tools, many of which were created to automate what I grew tired of doing manually. Others I created simply for fun.</p>
+      <p>Since 2010, I've been programming for work, for school, and for fun. My most prominent work includes a plethora of web apps and useful tools, many of which were created to automate what I grew tired of doing manually. Others I created simply for fun. With a Bachelor's degree in Computer Science under my belt, I aim to build apps that are enjoyable, functional, and adhere to best practices.</p>
 
       <h3>Why do you code?</h3>
 
-      <p>Besides the fact that I greatly enjoy coding, I make it my aim to code for Christ, who has always walked with me as my Lord, my Friend, and my Savior. I code because I believe the work of my hands glorifies God through the creativity I express, and ultimately, through the lives enriched by my projects.</p>
+      <p>Besides the fact that I thoroughly enjoy programming, I make it my aim to code for Christ, who has walked with me as my Lord, my Friend, and my Savior. I believe the work of my hands glorifies God through the creativity I express, and ultimately, through the lives enriched by my projects.</p>
 
       <h3>How many languages do you speak?</h3>
 
-      <p>I am fluent in over six million forms of... ha, just kidding. I am fluent in HTML5, CSS3, JavaScript, and PHP, since most of my projects are web-based. I'm also well-versed in Python, and I fiddle with Bash regularly in an effort to improve my developer workflow.</p>
+      <p>I am fluent in HTML5, CSS/Sass, and JavaScript/Node, and have recently immersed myself in TypeScript. I'm also well-versed in Python, and I fiddle with Bash regularly in an effort to improve my development workflow.</p>
 
       <h3>What is your favorite language?</h3>
 
-      <p>If I could choose only one, my favorite language would definitely be TypeScript because of how many bugs it saves me from every time I write it. Plus, I thoroughly enjoy working with the ES6 language features like promises, destructuring, and arrow functions.</p>
+      <p>If I could choose only one, my favorite language would definitely be TypeScript because of how many bugs it saves me from every time I write in it. Plus, I thoroughly enjoy working with the ES6 language features like promises, destructuring, and arrow functions.</p>
 
       <h3>What is your code editor of choice?</h3>
 
