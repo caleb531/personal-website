106b61f7b7d008492620eac30eece4397554c656
Caleb Evans <caleb@calebevans.me>
2022-02-21 19:04:02 -0800

    Fix NextJS dev WebSocket not loading in Safari

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
       {
         key: 'Content-Security-Policy',
         /* eslint-disable-next-line quotes */
-        value: "default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src data: https://fonts.gstatic.com; img-src 'self' data: https://www.gravatar.com https://www.google-analytics.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com; prefetch-src 'self'"
+        value: "default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src data: https://fonts.gstatic.com; img-src 'self' data: https://www.gravatar.com https://www.google-analytics.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://www.google-analytics.com; connect-src 'self' ws: https://www.google-analytics.com; prefetch-src 'self'"
       }
     ];
     // The HSTS header should only be sent for HTTPS websites; because localhost is server over plain HTTP, we do not want to enable HSTS there
