caleb531/personal-website

commit 6c8f72116a5661ec732f6e403799dbb13a651419

Implement project promotion/demotion logic

src/components/ProjectCategory.tsx
@@ -1,3 +1,4 @@
+import { keyBy } from 'lodash-es';
 import React from 'react';
 import Project from './Project';
 import { ProjectCategoryData, ProjectEntry } from './types';
@@ -5,6 +6,18 @@ import { ProjectCategoryData, ProjectEntry } from './types';
 type Props = { category: ProjectCategoryData, projects: ProjectEntry[] };
 
 function ProjectCategory({ category, projects }: Props) {
+  // Promote some projects to the top of the list
+  const topProjectsSet = new Set(category.topProjects);
+  // Demote some projects to the end of the list
+  const bottomProjectsSet = new Set(category.bottomProjects);
+  const projectsById = keyBy(projects, 'id');
+  // Re-sort projects list according to the promotion/demotion criteria defined
+  // for this category
+  projects = [
+    ...(category.topProjects || []).map((projectId) => projectsById[projectId]),
+    ...projects.filter((project) => !topProjectsSet.has(project.id) && !bottomProjectsSet.has(project.id)),
+    ...(category.bottomProjects || []).map((projectId) => projectsById[projectId])
+  ];
   return (
     <>
     {projects.length ? <section className="entry-list project-list">

src/components/types.d.ts
@@ -27,6 +27,8 @@ export interface ProjectGroups {
 export interface ProjectCategoryData {
   id: string;
   title: string;
+  topProjects?: string[];
+  bottomProjects?: string[];
 }
 interface ProjectCategoryMap {
   [key: string]: ProjectCategoryData;

commit 7987716eb41b1a252dbb6571e67bfcd3e0a6476a

Fix project search functionality throwing error
This commit fixes a regression introduced in 6c8f721.

src/components/ProjectCategory.tsx
@@ -14,9 +14,19 @@ function ProjectCategory({ category, projects }: Props) {
   // Re-sort projects list according to the promotion/demotion criteria defined
   // for this category
   projects = [
-    ...(category.topProjects || []).map((projectId) => projectsById[projectId]),
+    ...(category.topProjects || [])
+      .map((projectId) => projectsById[projectId])
+      // If the user searches for a project, then projectsById[projectId] might
+      // return undefined because topProjects may not include any projects that
+      // can be shown; to solve this, we filter out all falsy values
+      .filter(Boolean),
     ...projects.filter((project) => !topProjectsSet.has(project.id) && !bottomProjectsSet.has(project.id)),
-    ...(category.bottomProjects || []).map((projectId) => projectsById[projectId])
+    ...(category.bottomProjects || [])
+      .map((projectId) => projectsById[projectId])
+      // If the user searches for a project, then projectsById[projectId] might
+      // return undefined because bottomProjects may not include any projects
+      // that can be shown; to solve this, we filter out all falsy values
+      .filter(Boolean)
   ];
   return (
     <>