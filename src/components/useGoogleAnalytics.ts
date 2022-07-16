4ad3656c27819b0bba453f9f3b37dfe8988eccf5
Caleb Evans <caleb@calebevans.me>
2022-07-16 15:53:27 -0700

    Add Google Analytics 4 (GA4) tracking code

--- src/components/useGoogleAnalytics.ts
+++ src/components/useGoogleAnalytics.ts
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import ReactGA4 from 'react-ga4';
 import site from '../data/site.json';
 
 // The useGoogleAnalytics() hook is a simple abstraction around the react-ga
@@ -8,13 +9,14 @@ function useGoogleAnalytics() {
   // Initialize Google Analytics on the initial page load
   useEffect(() => {
     if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
-      ReactGA.initialize(site.googleAnalytics);
+      ReactGA.initialize(site.uaTrackingId);
+      ReactGA4.initialize(site.ga4TrackingId);
     }
   }, []);
   // Trigger a pageview even on the initial page load and when navigating to
   // another page on the site
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
       ReactGA.pageview(window.location.pathname + window.location.search);
     }
   });
