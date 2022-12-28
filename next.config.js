c9db97f2f219ee74a83deec4f59af1250d0df4f3
Caleb Evans <caleb@calebevans.me>
2022-12-28 15:38:39 -0800

    Remove CloudFlare from CSP
    It's no longer needed.

--- next.config.js
+++ next.config.js
@@ -33,8 +33,10 @@ const nextConfig = {
       },
       {
         key: 'Content-Security-Policy',
-        /* eslint-disable-next-line quotes */
-        value: "default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src data: https://fonts.gstatic.com; img-src 'self' data: https://www.gravatar.com https://www.google-analytics.com https://www.googletagmanager.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com https://www.google-analytics.com https://www.googletagmanager.com; connect-src 'self' ws: https://www.google-analytics.com https://www.googletagmanager.com; prefetch-src 'self'"
+        /* eslint-disable quotes */
+        value:
+          "default-src 'none'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src data: https://fonts.gstatic.com; img-src 'self' data: https://www.gravatar.com https://www.google-analytics.com https://www.googletagmanager.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com; connect-src 'self' ws: https://www.google-analytics.com https://www.googletagmanager.com; prefetch-src 'self'"
+        /* eslint-enable quotes */
       }
     ];
     // The HSTS header should only be sent for HTTPS websites; because localhost is server over plain HTTP, we do not want to enable HSTS there
