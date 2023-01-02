52a1d795cf72940894e0ed772945ceda3b43eec6
Caleb Evans <caleb@calebevans.me>
2023-01-02 13:55:20 -0800

    Add Projects page

---
+++ src/routes/projects/+page.server.ts
@@ -0,0 +1,14 @@
+import type { PageProps } from '../../components/types.d';
+import { getProjects } from '../../lib/entries';
+import type { PageServerLoad } from './$types';
+
+type Props = PageProps & { projects: ReturnType<typeof getProjects>};
+
+export const load = ((): Props => {
+  return {
+    id: 'projects',
+    title: 'Projects',
+    description: 'Apps and programs crafted by Caleb Evans, coder for Christ',
+    projects: getProjects()
+  };
+}) satisfies PageServerLoad;
