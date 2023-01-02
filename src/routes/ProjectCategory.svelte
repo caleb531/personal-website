7d461439ffd2688598f518f2484ee24cc7da70dc
Caleb Evans <caleb@calebevans.me>
2023-01-02 13:48:44 -0800

    Fix filtering of projects within categories
    The projectsById map was not getting updated as the list was being
    filtered.

--- src/routes/ProjectCategory.svelte
+++ src/routes/ProjectCategory.svelte
@@ -10,35 +10,39 @@
 	const topProjectsSet = new Set(category.topProjects);
 	// Demote some projects to the end of the list
 	const bottomProjectsSet = new Set(category.bottomProjects);
-	const projectsById = keyBy(projects, 'id');
+	let projectsById: { [key: string]: ProjectEntry };
 	// Re-sort projects list according to the promotion/demotion criteria defined
 	// for this category
-	projects = [
-		...(category.topProjects || [])
-			.map((projectId) => projectsById[projectId])
-			// If the user searches for a project, then projectsById[projectId] might
-			// return undefined because topProjects may not include any projects that
-			// can be shown; to solve this, we filter out all falsy values
-			.filter(Boolean),
-		...projects.filter(
-			(project) => !topProjectsSet.has(project.id) && !bottomProjectsSet.has(project.id)
-		),
-		...(category.bottomProjects || [])
-			.map((projectId) => projectsById[projectId])
-			// If the user searches for a project, then projectsById[projectId] might
-			// return undefined because bottomProjects may not include any projects
-			// that can be shown; to solve this, we filter out all falsy values
-			.filter(Boolean)
-	];
+	let sortedProjects: ProjectEntry[];
+	$: {
+		projectsById = keyBy(projects, 'id');
+		sortedProjects = [
+			...(category.topProjects || [])
+				.map((projectId) => projectsById[projectId])
+				// If the user searches for a project, then projectsById[projectId] might
+				// return undefined because topProjects may not include any projects that
+				// can be shown; to solve this, we filter out all falsy values
+				.filter(Boolean),
+			...projects.filter(
+				(project) => !topProjectsSet.has(project.id) && !bottomProjectsSet.has(project.id)
+			),
+			...(category.bottomProjects || [])
+				.map((projectId) => projectsById[projectId])
+				// If the user searches for a project, then projectsById[projectId] might
+				// return undefined because bottomProjects may not include any projects
+				// that can be shown; to solve this, we filter out all falsy values
+				.filter(Boolean)
+		];
+	}
 </script>
 
-{#if projects.length}
+{#if sortedProjects.length}
 	<section class="entry-list project-list">
 		<h3 class="entry-list-category-title project-list-category-title">
 			{category.title}
 		</h3>
 
-		{#each projects as project}
+		{#each sortedProjects as project}
 			<Project {project} />
 		{/each}
 	</section>
