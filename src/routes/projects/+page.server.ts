9e3d675a32755437709f0bcf0ec70b774246b49c
Caleb Evans <caleb@calebevans.me>
2023-01-02 16:42:56 -0800

    Remove outstanding use of PageProps on projects page server load()

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,14 +1,11 @@
-import type { PageProps } from '../../components/types.d';
 import { getProjects } from '../../lib/entries';
 import type { PageServerLoad } from './$types';
 
-type Props = PageProps & { projects: ReturnType<typeof getProjects> };
-
-export const load = ((): Props => {
+export const load = (() => {
 	return {
 		id: 'projects',
 		title: 'Projects',
 		description: 'Apps and programs crafted by Caleb Evans, coder for Christ',
 		projects: getProjects()
 	};
-}) satisfies PageServerLoad<Props>;
+}) satisfies PageServerLoad;
