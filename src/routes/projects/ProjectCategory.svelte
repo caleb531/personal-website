bfb86731e27bd07137e3ecba850c4c7b53971da6
Caleb Evans <caleb@calebevans.me>
2023-01-03 14:26:21 -0800

    Reflow comments in ProjectCategory component

--- src/routes/projects/ProjectCategory.svelte
+++ src/routes/projects/ProjectCategory.svelte
@@ -19,18 +19,20 @@
     sortedProjects = [
       ...(category.topProjects || [])
         .map((projectId) => projectsById[projectId])
-        // If the user searches for a project, then projectsById[projectId] might
-        // return undefined because topProjects may not include any projects that
-        // can be shown; to solve this, we filter out all falsy values
+        // If the user searches for a project, then projectsById[projectId]
+        // might return undefined because topProjects may not include any
+        // projects that can be shown; to solve this, we filter out all falsy
+        // values
         .filter(Boolean),
       ...projects.filter(
         (project) => !topProjectsSet.has(project.id) && !bottomProjectsSet.has(project.id)
       ),
       ...(category.bottomProjects || [])
         .map((projectId) => projectsById[projectId])
-        // If the user searches for a project, then projectsById[projectId] might
-        // return undefined because bottomProjects may not include any projects
-        // that can be shown; to solve this, we filter out all falsy values
+        // If the user searches for a project, then projectsById[projectId]
+        // might return undefined because bottomProjects may not include any
+        // projects that can be shown; to solve this, we filter out all falsy
+        // values
         .filter(Boolean)
     ];
   }
