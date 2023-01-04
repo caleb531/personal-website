cc8a1a263e70c8cf33f8a002f82e6436923bcfd3
Caleb Evans <caleb@calebevans.me>
2023-01-04 14:56:54 -0800

    Make entries server code pre-bundle Markdown data

--- src/lib/entries.server.ts
+++ src/lib/entries.server.ts
@@ -1,32 +1,46 @@
-import fs from 'fs';
-import { glob } from 'glob-promise';
 import matter from 'gray-matter';
 import path from 'path';
 import type { ContactLinkEntry, Entry, ProjectEntry, WebsiteEntry } from '../routes/types';
 
+type EntryType = 'contact_link' | 'project' | 'website';
+type GlobMap = Record<string, () => Promise<unknown>>;
+type EntriesByTypeMap = Record<EntryType, GlobMap>;
+
+// A map where each key name a specific entry type (e.g. project) and each value
+// is a map of that entry type's files (for this submap, the key is the filepath
+// and the value is a function which returns the contents of the file)
+const entriesByType: EntriesByTypeMap = {
+  contact_link: import.meta.glob('../contact-links/*.md', { as: 'raw' }),
+  project: import.meta.glob('../projects/*.md', { as: 'raw' }),
+  website: import.meta.glob('../websites/*.md', { as: 'raw' })
+};
+
 // Retrieve a list of entries for the given entry type, optionally specifying a
 // callback function that dynamically defines additional properties to
 // initialize the entry with
-export async function getEntries<SubEntry extends Entry>(entryType: string): Promise<SubEntry[]> {
-  const entryDirectory = path.join(process.cwd(), 'src', entryType);
-  const entryPaths = glob.sync(`${entryDirectory}/*.md`);
-  return entryPaths.map((entryPath) => {
-    const entryId = path.basename(entryPath, '.md');
-    return {
-      id: entryId,
-      ...matter(fs.readFileSync(entryPath)).data
-    } as SubEntry;
-  });
+export async function getEntries<SubEntry extends Entry>(
+  entryType: EntryType
+): Promise<SubEntry[]> {
+  const entryPairs = Object.entries(entriesByType[entryType]);
+  return Promise.all(
+    entryPairs.map(async ([entryPath, getEntryContents]: [string, GlobMap[1]]) => {
+      const entryId = path.basename(entryPath, '.md');
+      return {
+        id: entryId,
+        ...matter(String(await getEntryContents())).data
+      } as SubEntry;
+    })
+  );
 }
 
 export async function getProjects(): Promise<ProjectEntry[]> {
-  return getEntries<ProjectEntry>('projects');
+  return getEntries<ProjectEntry>('project');
 }
 
 export async function getContactLinks(): Promise<ContactLinkEntry[]> {
-  return getEntries<ContactLinkEntry>('contact-links');
+  return getEntries<ContactLinkEntry>('contact_link');
 }
 
 export async function getWebsiteEntries(): Promise<WebsiteEntry[]> {
-  return getEntries<WebsiteEntry>('websites');
+  return getEntries<WebsiteEntry>('website');
 }
