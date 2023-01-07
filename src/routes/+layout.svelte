17db7c69d808793bada85f405dc8f6cabc0e77bb
Caleb Evans <caleb@calebevans.me>
2023-01-07 08:19:15 -0800

    Add comment explaining need for keying page element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,10 @@
 
 <main data-page-id={$page.data.id}>
   <Header />
+  <!-- Keying the page element fixes a bug where the page heading would change
+  before the rest of the new page content loads in; this was mostly caused by
+  the introduction of CSS transitions on my Projects page (which must complete
+  before SvelteKit can finish navigating to the new page) -->
   {#key $page}
     <article class="page" id="page">
       {#if $page.data.title}
