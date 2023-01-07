3bae8620514cb908ee863cbccf6655d9912bd72d
Caleb Evans <caleb@calebevans.me>
2023-01-07 08:53:13 -0800

    Close mobile nav when clicking homepage link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import { resizeGravatar } from '$lib/gravatar';
+  import { writable } from 'svelte/store';
   import Navigation from './Navigation.svelte';
   let { gravatarUrl, site } = $page.data;
 
   const headerImageSize = 60;
   const headerGravatarUrl = resizeGravatar(gravatarUrl, headerImageSize);
   const headerGravatarUrlRetina = resizeGravatar(gravatarUrl, headerImageSize * 2);
+
+  let isNavOpen = writable(false);
+
+  function closeNav() {
+    $isNavOpen = false;
+  }
 </script>
 
 <header class="site-header">
-  <a href="/" class="site-title-link" rel="home">
+  <a href="/" class="site-title-link" rel="home" on:click={closeNav}>
     <img
       class="site-header-image"
       src={headerGravatarUrl}
@@ -21,5 +28,5 @@
     />
     <h1 class="site-title">{site.title}</h1>
   </a>
-  <Navigation />
+  <Navigation {isNavOpen} on:click-nav-link={closeNav} />
 </header>
--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import { createEventDispatcher } from 'svelte';
+  import type { Writable } from 'svelte/store';
   import navigation from '../data/navigation.json';
 
-  let isNavOpen = false;
+  const dispatch = createEventDispatcher();
 
+  export let isNavOpen: Writable<boolean>;
   function toggleNav() {
-    isNavOpen = !isNavOpen;
+    $isNavOpen = !$isNavOpen;
   }
-  function closeNav() {
-    isNavOpen = false;
-  }
+
   // Normalize the given URL pathname (e.g. /about/me -> about/me)
   function normalizePathname(pathname: string) {
     return pathname.replace(/(^\/)|(\/$)/gi, '');
@@ -19,14 +20,14 @@
   }
 </script>
 
-<nav class="site-header-nav" class:site-header-nav-open={isNavOpen}>
+<nav class="site-header-nav" class:site-header-nav-open={$isNavOpen}>
   <button class="site-header-nav-toggle" aria-label="Menu" on:click={toggleNav}>
     <img src="/icons/nav-toggle.svg" alt="Toggle Navigation" />
   </button>
   <ul class="site-header-nav-list">
     {#each navigation as navigationLink (navigationLink.url)}
       <li class:is-current-page={isCurrentPage(navigationLink, $page)}>
-        <a href={navigationLink.url} on:click={closeNav}>
+        <a href={navigationLink.url} on:click={() => dispatch('click-nav-link')}>
           {navigationLink.title}
         </a>
       </li>
