c99a626b521f9ac7b2dd36f8992df43fed05a544
Caleb Evans <caleb@calebevans.me>
2023-02-11 13:43:14 -0800

    Remove obsolete plausible global type declaration
    Made obsolete by 8b1d159 a few weeks ago.

--- src/routes/globals.d.ts
+++ src/routes/globals.d.ts
@@ -1,6 +1,3 @@
-// Declare missing global variable for Plausible Analytics
-declare const plausible: (eventName: string, eventData?: { props: object }) => void;
-
 // Fixes a "Cannot find module '...' or its corresponding type declarations."
 // issue when using vite-imagetools (source:
 // <https://github.com/JonasKruckenberg/imagetools/issues/160#issuecomment-1009292026>)
