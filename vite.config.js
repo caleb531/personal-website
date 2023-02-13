9be56e73c54aeb2602266cfa883fe7df4852258e
Caleb Evans <caleb@calebevans.me>
2023-02-12 16:50:43 -0800

    Allow conditionally enabling sourcemaps during build
    This will allow us to opt into sourcemaps for any environment we choose
    (e.g. on staging, but not production).

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ const config = {
   plugins: [sveltekit(), imagetools()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  build: {
+    // Allow us to conditionally enable sourcemaps on a per-environment basis
+    // (this does not apply to development mode, since we are not building)
+    sourcemap: Boolean(process.env.ENABLE_SOURCEMAPS)
   }
 };
 
