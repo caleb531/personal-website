8a60428ec90e0a32649e68530ca91f3cc0b653f5
Caleb Evans <caleb@calebevans.me>
2023-02-12 17:02:27 -0800

    Remove testing config from vite config
    There are no plans to write any tests for this website.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { imagetools } from 'vite-imagetools';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit(), imagetools()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
-  },
   build: {
     // Allow us to conditionally enable sourcemaps on a per-environment basis
     // (this does not apply to development mode, since we are not building)
