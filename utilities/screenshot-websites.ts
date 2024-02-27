3b82f0747e82d071d80fcae2ab96ee807d47a5ec
Caleb Evans <caleb@calebevans.me>
2024-02-26 18:03:17 -0800

    Remove obsolete 'headless' Puppeteer option

--- utilities/screenshot-websites.ts
+++ utilities/screenshot-websites.ts
@@ -20,7 +20,7 @@ function createWebsiteImageDirectory(): Promise<string | undefined> {
 async function generateScreenshots(websiteConfigFilePaths: string[]): Promise<void> {
   await createWebsiteImageDirectory();
   console.log('launching browser...');
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch();
 
   // Generate screenshot for each portfolio website that has configuration
   await Promise.all(
