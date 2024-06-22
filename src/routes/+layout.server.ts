7aa289908c5929f958ebb09d16a7ac4205446db3
Caleb Evans <caleb@calebevans.me>
2024-06-22 10:09:45 -0700

    Enforce trailing slashes for static build of site

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,11 @@
 import { getContactLinks } from '$lib/entries.server';
 
 export const prerender = Boolean(process.env.USE_STATIC_ADAPTER);
+// Without enforcing trailing slashes, when SvelteKit builds using the static
+// adapter, pages will be written to the build/ directory as mypage.html rather
+// than mypage/index.html (source:
+// <https://kit.svelte.dev/docs/page-options#trailingslash>)
+export const trailingSlash = 'always';
 
 // Define props that should be globally available across all pages
 export async function load(locals) {
