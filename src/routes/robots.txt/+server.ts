56e2c13ffc57933c14127608cde73199377c33db
Caleb Evans <caleb@calebevans.me>
2024-06-23 14:05:45 -0700

    Streamline robots.txt logic with ternary
    So that we can embrace immutable patterns (i.e. assignment at
    declaration time) constants) rather than lazy assignment.

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -4,11 +4,8 @@ export const prerender = true;
 // variable; this is so we can allow bots for the production site, while
 // disallowing bots on the staging site
 export function GET() {
-  let directives;
-  if (process.env.DISALLOW_BOTS) {
-    directives = ['User-agent: *', 'Disallow: /'];
-  } else {
-    directives = ['User-agent: *', 'Disallow:'];
-  }
+  const directives = process.env.DISALLOW_BOTS
+    ? ['User-agent: *', 'Disallow: /']
+    : ['User-agent: *', 'Disallow:'];
   return new Response(directives.join('\n'));
 }
