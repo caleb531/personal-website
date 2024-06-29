78c483ba259d6fb19a93fce9a6f6a93f7abd4bb1
Caleb Evans <caleb@calebevans.me>
2024-06-29 15:44:41 -0700

    Clear input on pointerup of button, rather than pointerdown
    We successfully figured out how to accomplish this by leveraging both
    events.

--- src/routes/SearchInput.svelte
+++ src/routes/SearchInput.svelte
@@ -12,14 +12,15 @@
 
 <div class="search-input-container">
   <input type="search" {name} {id} bind:value {placeholder} aria-label={ariaLabel} />
-  <!-- pointerdown must be used (instead of pointerup) for the Clear Search
-  button to ensure that the preventDefault keeps the input focused after button
-  click -->
+  <!-- pointerdown|preventDefault must be used in conjunction with pointerup to
+  ensure that the search input doesn't lose focus when the user initially
+  depresses the button -->
   <button
     type="button"
     class="search-input-clear-button"
     class:hidden={value === ''}
-    on:pointerdown|preventDefault={clearSearch}
+    on:pointerdown|preventDefault
+    on:pointerup|preventDefault={clearSearch}
     title="Clear Search Query"
   >
     <img src="/icons/clear.svg" alt="Clear Search Query" />
