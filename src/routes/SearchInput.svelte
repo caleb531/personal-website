39d4af7c9a5cdc7c7ab643795904036ab72ab935
Caleb Evans <caleb@calebevans.me>
2024-06-29 15:34:37 -0700

    Require explicitly passing 'name' prop to SearchInput

--- src/routes/SearchInput.svelte
+++ src/routes/SearchInput.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let id: string;
-  export let name: string = 'search';
+  export let name: string;
   export let value: string;
   export let placeholder: string = '';
   export let ariaLabel: string;
