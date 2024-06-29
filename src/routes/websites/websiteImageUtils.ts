1138c919fe20aff18cf22e77d9cc319d2ececedf
Caleb Evans <caleb@calebevans.me>
2024-06-29 12:56:01 -0700

    Fix resizing of website entry images
    As of vite-imagetools v5, the 'width' property is now called 'w'
    (source:
    <https://github.com/JonasKruckenberg/imagetools/blob/main/packages/vite/CHANGELOG.md#major-changes-2>).

--- src/routes/websites/websiteImageUtils.ts
+++ src/routes/websites/websiteImageUtils.ts
@@ -1,13 +1,16 @@
 import type { WebsiteEntry } from '$routes/types';
 
-type ImagetoolsResult = { default: string[] };
-type GlobUrlMap = Record<string, ImagetoolsResult>;
+type GlobUrlMap = Record<string, string[]>;
 
 // Resize the website images at build time so that the appropriate (smaller)
 // versions of each website image can be served
 export const resizedWebsiteUrlMap: GlobUrlMap = import.meta.glob('../../images/websites/*.jpg', {
-  // Generate additional sizes for each pregenerated website image
-  query: { width: '256;512' },
+  // Generate additional sizes for each pregenerated website image (note that as
+  // of vite-imagetools v5, the 'width' query parameter has been renamed to 'w';
+  // see
+  // <https://github.com/JonasKruckenberg/imagetools/blob/main/packages/vite/CHANGELOG.md#major-changes-2>)
+  query: { w: '256;512' },
+  import: 'default',
   // Resolve each import promise and store the final values
   eager: true
 });
@@ -15,11 +18,11 @@ export const resizedWebsiteUrlMap: GlobUrlMap = import.meta.glob('../../images/w
 // Retrieve the URL to the regular-sized thumbnail for this website
 export function getWebsite1xThumbnailUrl(website: WebsiteEntry): string {
   const imagePath = `../../images/websites/${website.id}.jpg`;
-  return resizedWebsiteUrlMap[imagePath].default[0];
+  return resizedWebsiteUrlMap[imagePath][0];
 }
 
 // Retrieve the URL to the high-density (Retina) thumbnail for this website
 export function getWebsite2xThumbnailUrl(website: WebsiteEntry): string {
   const imagePath = `../../images/websites/${website.id}.jpg`;
-  return resizedWebsiteUrlMap[imagePath].default[1];
+  return resizedWebsiteUrlMap[imagePath][1];
 }
