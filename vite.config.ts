4626bb3fca920c5da5b139784db812fd66abe05f
Caleb Evans <caleb@calebevans.me>
2024-07-01 15:48:29 -0700

    Replace SVGO plugin with vite-plugin-image-optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
-import svgo from './plugins/vite-plugin-svgo';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 const config: UserConfig = {
-  plugins: [sveltekit(), imagetools(), svgo()],
+  plugins: [
+    sveltekit(),
+    imagetools(),
+    // Since vite-plugin-image-optimizer does not yet support image resizing
+    // (although we hope this will be added soon via
+    // <https://github.com/FatehAK/vite-plugin-image-optimizer/pull/35>), we
+    // must still use vite-imagetools to handle resizing of JPEGs, while also
+    // using vite-plugin-image-optimizer to optimize SVGs
+    ViteImageOptimizer({ test: /\.(svg)$/i })
+  ],
   build: {
     // Allow us to conditionally enable sourcemaps on a per-environment basis
     // (this does not apply to development mode, since we are not building)
